9a32b9208911f7b95fbc1833b9cc7dbd35c3a3a8
Tor Børve Rasmussen <rasmustor18@gmail.com>
2023-07-04 16:31:08 +0200

    testing stuff

--- include/LaunchUnit.h
+++ include/LaunchUnit.h
@@ -2,17 +2,21 @@
 
 #include <Servo.h>
 
-#define LU_SERVO_LOADED 45
-#define LU_SERVO_TRIGGERED 90
+#define LU_SERVO_LOADED 0
+#define LU_SERVO_TRIGGERED 180
 
 class LaunchUnit {
 public:
+    enum class State : uint8_t {FIRED, LOADED, ERROR};
     LaunchUnit(uint8_t servoPin);
     bool isLoaded() const;
     void launch();
-    // void load();
-
+    void load();
+    void update();
 private:
+    State _state;
     Servo _triggerServo;
     uint8_t _servoPin;
+    uint8_t _loadedBtnPin;
+    uint8_t _firedBtnPin;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,55 @@
 
 #include "Button.h"
 #include "LaunchUnit.h"
+#include "cmath"
 
-#define BUTTON_PIN 20
-#define SERVO_PIN 21
+#define SAFETY_SERVO_PIN 2
+#define SAFETY_BUTTON_PIN 3
+#define TRIGGER_BUTTON_PIN 20
+#define TRIGGER_SERVO_PIN 21
+#define SAFETY_SERVO_MIN_ANGLE 180
+#define SAFETY_SERVO_MAX_ANGLE 130
 
-Button btn{BUTTON_PIN};
-LaunchUnit launchUnit{SERVO_PIN};
+Button triggerBtn{TRIGGER_BUTTON_PIN};
+Button safetyBtn{SAFETY_BUTTON_PIN};
+Servo triggerServo;
+// LaunchUnit launchUnit{TRIGGER_SERVO_PIN};
+Servo safetyServo;
+
 
 void setup() {
     delay(1000);
     Serial.println("Setup");
+    triggerServo.attach(TRIGGER_SERVO_PIN);
+    triggerServo.write(LU_SERVO_LOADED);
+    safetyServo.attach(SAFETY_SERVO_PIN);
+    safetyServo.write(SAFETY_SERVO_MIN_ANGLE);
 }
 
 void loop() {
-    btn.poll(millis());
-    if (btn.wasPressed()) {
-        Serial.println("Btn pressed");
-        launchUnit.launch();
+    uint32_t now = millis();
+    safetyBtn.poll(now);
+    triggerBtn.poll(now);
+    if (triggerBtn.wasPressed()) {
+        Serial.println("Trigger Btn pressed");
+        if (triggerServo.read() == LU_SERVO_LOADED) {
+            triggerServo.write(LU_SERVO_TRIGGERED);
+        } else {
+            triggerServo.write(LU_SERVO_LOADED);
+        }
+        // launchUnit.launch();
+        triggerBtn.reset();
+        delay(3000);
+    }
+    if (safetyBtn.wasPressed()) {
+        Serial.println("Safety btn pressed");
+
+        if (std::fabs(safetyServo.read() - SAFETY_SERVO_MIN_ANGLE) < 5) {
+            safetyServo.write(SAFETY_SERVO_MAX_ANGLE);
+        } else {
+            safetyServo.write(SAFETY_SERVO_MIN_ANGLE);
+        }
+        safetyBtn.reset();
+        delay(3000);
     }
 }
