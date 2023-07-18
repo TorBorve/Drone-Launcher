dc4c013d2e926323985c3e80446e41692bdd297c
Tor Børve Rasmussen <rasmustor18@gmail.com>
2023-07-18 15:16:13 +0200

    cleaning up main

--- embedded_sw/src/main.cpp
+++ embedded_sw/src/main.cpp
@@ -1,42 +1,20 @@
 #include <Arduino.h>
 
-#include "Buttons.h"
-#include "LaunchUnit.h"
 #include "Pins.h"
-
-// #include <TeensyThreads.h>
-
-#define SAFETY_SERVO_PIN 1
-// #define SAFETY_BUTTON_PIN 3
-// #define TRIGGER_BUTTON_PIN 20
-#define TRIGGER_SERVO_PIN 21
-// #define SAFETY_SERVO_MIN_ANGLE 163
-// #define SAFETY_SERVO_MAX_ANGLE 150
-
-#define REAR_SWITCH_PIN 3
-#define SAFETY_SWITCH_PIN 4
-#define FRONT_SWITCH_PIN 5
-
-#define LED_PIN     2
-#define NUM_LEDS    6
-
-CRGB leds[NUM_LEDS];
-
-LaunchUnit launchUnit{TRIGGER_SERVO_PIN, SAFETY_SERVO_PIN, REAR_SWITCH_PIN, SAFETY_SWITCH_PIN, FRONT_SWITCH_PIN, leds[0]};
+#include "LaunchSystem.h"
+#include "Communicator.h"
 
 void setup() {
     delay(3000);
-    Serial.println("Setup started");
+    // Serial.println("Setup started");
     pins::setup();
-    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-    launchUnit.init();
-    FastLED.show();
+    launchSystem.init();
+    communicator.init();
     Serial.println("Setup complete");
-    launchUnit.load();
 }
 
 void loop() {
     uint32_t now = millis();
-    launchUnit.update(now);
-    FastLED.show();
+    launchSystem.update(now);
+    communicator.update(now);
 }
