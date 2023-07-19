091af02fffc000eacbbbc0c6be94a725b9eae86b
Tor Børve Rasmussen <rasmustor18@gmail.com>
2023-07-19 13:44:10 +0200

    fixed bugs and code quality

--- embedded_sw/include/BatteryMonitor.h
+++ embedded_sw/include/BatteryMonitor.h
@@ -2,23 +2,22 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
-#define BM_NUM_LED 5
+#define BM_NUM_LED 6
 
 class BatteryMonitor {
     private:
-        int _pin_value;
-        float _battery_percentage;
-        float _voltage;
-        float _percentage;
         void readBat();
-        CRGB _led_array[BM_NUM_LED];
+        uint8_t _batMeasPin;
+        float _voltage;
+        CRGB _ledArray[BM_NUM_LED];
         uint32_t _prevUpdate;
 
     public:
+        static float voltageToPercent(float voltage);
         void update(uint32_t now);
         void init();
         float getVoltage() {return _voltage;}
-        float getPercent() {return _percentage;}
+        float getPercent() {return voltageToPercent(_voltage);}
         BatteryMonitor();
 };
 
--- embedded_sw/src/BatteryMonitor.cpp
+++ embedded_sw/src/BatteryMonitor.cpp
@@ -1,35 +1,36 @@
 #include "BatteryMonitor.h"
 
+#include "Pins.h"
+
 
 #define PIN_BAT_LEDS 25
 #define PIN_BAT_MEAS 23
+#define BAT_SCALE 7.835306
 #define MIN_BAT_VOLTAGE (3.6 * 6)
 #define MAX_BAT_VOLTAGE (4.2 * 6)
 #define BRIGHTNESS  64
-#define LED_TYPE    WS2811
+#define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
-#define UPDATE_INTERVAL 1000
+#define UPDATE_INTERVAL 10
 
 
 BatteryMonitor batteryMonitor;
 
-BatteryMonitor::BatteryMonitor() {
-    _pin_value = -1;
-    _voltage = -1;
-    _percentage = -1;
-    _prevUpdate = 0;
+BatteryMonitor::BatteryMonitor() : _batMeasPin{PIN_BAT_MEAS}, _voltage{0}, _prevUpdate{0} {
     for (int i = 0; i < BM_NUM_LED; i++) {
-        _led_array[i] = CRGB::Black;
+        _ledArray[i] = CRGB::Black;
     }
 }
 
 void BatteryMonitor::init() {
     for (int i = 0; i < BM_NUM_LED; i++) {
-        _led_array[i] = CRGB::White;
+        _ledArray[i] = CRGB::White;
     }
-    FastLED.addLeds<LED_TYPE, PIN_BAT_LEDS, COLOR_ORDER>(_led_array, BM_NUM_LED).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(  BRIGHTNESS );
+    FastLED.addLeds<LED_TYPE, PIN_BAT_LEDS, COLOR_ORDER>(_ledArray, BM_NUM_LED).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(BRIGHTNESS);
     FastLED.show();
+
+    pinModeExt(_batMeasPin, INPUT);
     readBat();
 }
 
@@ -42,11 +43,12 @@ void BatteryMonitor::update(uint32_t now) {
     readBat();
 
     // Update LED value array
+    float percentage = voltageToPercent(_voltage);
     for (int i = 0; i < BM_NUM_LED; i++) {
-        if ((1.0 / BM_NUM_LED) * i < _percentage) {
-            _led_array[i] = CRGB::Green;
+        if ((1.0 / BM_NUM_LED) * i < percentage) {
+            _ledArray[i] = CRGB::Green;
         } else {
-            _led_array[i] = CRGB::Red;
+            _ledArray[i] = CRGB::Red;
         }
     }
     
@@ -55,7 +57,13 @@ void BatteryMonitor::update(uint32_t now) {
 }
 
 void BatteryMonitor::readBat() {
-    _pin_value = analogRead(PIN_BAT_MEAS);
-    _voltage = _pin_value * (3.3 / 1024.0) * (940.0 / 120.0);
-    _percentage = (_voltage - MIN_BAT_VOLTAGE) / (MAX_BAT_VOLTAGE - MIN_BAT_VOLTAGE);
+    _voltage = analogRead(PIN_BAT_MEAS) * (3.3 / 1024.0) * BAT_SCALE;
+    Serial.printf("Pin: %d\n", analogRead(PIN_BAT_MEAS));
+    Serial.printf("Pin Volatage: %f\n", analogRead(PIN_BAT_MEAS) * (3.3 / 1024.0));
+    Serial.printf("Battery voltage: %f\n", _voltage);
+    Serial.printf("Battery percentage: %f\n", voltageToPercent(_voltage));
+}
+
+float BatteryMonitor::voltageToPercent(float voltage) {
+    return min(1, max(0,(voltage - MIN_BAT_VOLTAGE) / (MAX_BAT_VOLTAGE - MIN_BAT_VOLTAGE)));
 }
