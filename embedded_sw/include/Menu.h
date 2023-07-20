4485b33ae8a15acffd645271319ab9b9e28f6d82
Tor Børve Rasmussen <rasmustor18@gmail.com>
2023-07-20 09:46:08 +0200

    first version simple menu

--- embedded_sw/include/Menu.h
+++ embedded_sw/include/Menu.h
@@ -1,67 +1,39 @@
 #pragma once
 
 #include "Adafruit_LiquidCrystal.h"
-#include "MD_Menu.h"
-#include <array>
-#include <string>
-
 #include "Buttons.h"
-#include "LaunchSystem.h"
+#include "MD_Menu.h"
 
 class Menu {
    public:
     Menu(uint8_t backBtnPin, uint8_t enterBtnPin, uint8_t upBtnPin, uint8_t downBtnPin);
-   private:
-    // enum class BtnType : uint8_t {
-    //     BACK,
-    //     ENTER,
-    //     UP,
-    //     DOWN,
-    // };
-    // enum class MenuScreen : uint8_t {
-    //     SLEEP,
-    //     MAIN,
-    //     LOAD,
-    //     UNLOAD,
-    //     STATUS,
-    //     SETTINGS,
-    // };
-    void updateLcdScreen();
-    // void updateMenuScreen(BtnType event);
+    void init();
+    void update(uint32_t now);
 
-    Adafruit_LiquidCrystal lcd{0};
+   private:
+    static MD_Menu::userNavAction_t navigation(uint16_t &incDelta);
+    static bool display(MD_Menu::userDisplayAction_t action, char *msg);
+    static MD_Menu::value_t *cbLoadDroneId(MD_Menu::mnuId_t id, bool bGet);
+    static MD_Menu::value_t *cbLoadLaunchUnitId(MD_Menu::mnuId_t id, bool bGet);
+    static MD_Menu::value_t *cbLoadConfirm(MD_Menu::mnuId_t id, bool bGet);
+    static MD_Menu::value_t *cbUnloadLaunchUnitId(MD_Menu::mnuId_t id, bool bGet);
+     static MD_Menu::value_t *cbUnloadConfirm(MD_Menu::mnuId_t id, bool bGet);
+
+    static const MD_Menu::mnuHeader_t mnuHdr[];
+    static const MD_Menu::mnuItem_t mnuItm[];
+    static const MD_Menu::mnuInput_t mnuInp[];
+
+    MD_Menu _menu;
+    uint8_t _loadDroneId;
+    uint8_t _loadLaunchUnitId;
+    uint8_t _unloadLaunchUnitId;
+    MD_Menu::value_t _vbuf;
+    Adafruit_LiquidCrystal _lcd;
     Button _backBtn;
     Button _enterBtn;
     Button _upBtn;
     Button _downBtn;
-    // MenuScreen _currentMenuScreen;
-    // const std::array<std::array<std::string, 2>, 5> _menuScreens = {{
-    //     {"Drone Launcher", ""},
-    //     {"Load", "Unload"},
-    //     {"Fire", "Cancel"},
-    //     {"Status", "Back"},
-    //     {"Settings", "Back"},
-    // }};
-
-    const MD_Menu::mnuHeader_t mnuHdr[] = {
-        {10, "Drone Launcher", 10, 12, 0},
-        {11, "Load", x, x, 0},
-        {12, "Unload", x, x, 0}
-    };
-
-    const MD_Menu::mnuItem_t mnuItm[] = {
-        {10, "Input Test", MD_Menu::MNU_MENU, 10},
-        {20, "Drone Id", MD_Menu::MNU_INPUT, x},
-        {21, "Launch Unit", MD_Menu::MNU_INPUT, x},
-        {22, "Start Load", MD_Menu::MNU_INPUT}
-    };
-
-    const MD_Menu::mnuInput_t mnuInp[] = 
-        // Load submenu
-        {10, "UInt8", MD_Menu::INP_INT, cbfunc, 4, 0, 10, 10, nullptr}, // Select Drone Id
-        {11, "UInt8", MD_Menu::INP_INT, cbfunc, 4, 0, 10, 10, nullptr}, // Select Launch Unit
-        {12, "Confirm", MD_Menu::INP_RUN, cbfunc, 4, 0, 10, 10, nullptr}, // Confirm Load
-
-    };
-
+    uint32_t _prevUpdate;
 };
+
+extern Menu menu;
