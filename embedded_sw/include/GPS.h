1e3bb665255566bfd4b82e21837ca096a28d13aa
Samuel Klouman Stoknes <samuel.stoknes@gmail.com>
2023-07-20 15:21:11 +0200

    Small Formatting Changes

--- embedded_sw/include/GPS.h
+++ embedded_sw/include/GPS.h
@@ -6,9 +6,16 @@ typedef struct PositionArray {
     double alt; // meters
 } PositionArray;
 
+typedef struct LastUpdateArray {
+    uint32_t time_since_last_loc_update;
+    uint32_t time_since_last_alt_update;
+    uint32_t time_since_last_date_update;
+    uint32_t time_since_last_time_update;
+    uint32_t time_since_last_speed_update;
+} LastUpdateArray;
+
 class GPS {
     private:
-        void pingGPS();
         uint32_t _prevUpdate;
         uint8_t _rx;
         uint8_t _tx;
@@ -19,12 +26,24 @@ class GPS {
         uint32_t _last_alt_update;
         TinyGPSPlus tiny_gps;
         TinyGPSDate _date;
+        uint32_t _last_date_update;
         TinyGPSTime _time;
+        uint32_t _last_time_update;
+        TinyGPSSpeed _speed;
+        uint32_t _last_speed_update;
 
     public:
         void update(uint32_t now);
         void init();
-        PositionArray getPos() {return PositionArray{_lat, _long, _alt};}
+        bool status(uint32_t now);
+        PositionArray getPos() {return PositionArray{_lat,
+                                                     _long, 
+                                                     _alt};}
+        LastUpdateArray getUpdateTimes(uint32_t now) {return LastUpdateArray{now - _last_loc_update,
+                                                                             now - _last_alt_update,
+                                                                             now - _last_date_update,
+                                                                             now - _last_time_update,
+                                                                             now - _last_speed_update};}
         TinyGPSTime getTime() {return _time;}
         TinyGPSDate getDate() {return _date;}
         GPS();
--- embedded_sw/src/GPS.cpp
+++ embedded_sw/src/GPS.cpp
@@ -8,20 +8,16 @@
 #define UPDATE_INTERVAL 1000
 #define GPS_RX 21 // We RECIEVE - READ HERE
 #define GPS_TX 20 // GPS RECIEVES - WRITE HERE
-#define GPSBaud 9600
+#define GPSBaud 38400
 #define GPS_SERIAL Serial4
+#define MAX_GPS_TIMEOUT 10000
 
 GPS gps;
 
-
-GPS::GPS() : _rx{GPS_RX}, _tx{GPS_TX}, _prevUpdate{0}, tiny_gps{} {
-    _lat = 0;
-    _long = 0;
-    _last_loc_update = 0;
-    _alt = 0;
-    _last_alt_update = 0;
-    return;
-}
+GPS::GPS() : _prevUpdate(0),_rx(GPS_RX), _tx(GPS_TX), _lat(0), _long(0), _last_loc_update(0),
+            _alt(0), _last_alt_update(0), tiny_gps(), _date(), _last_date_update(0), _time(), _last_time_update(0),
+            _speed(), _last_speed_update(0) {
+    }
 
 void GPS::init() {
     GPS_SERIAL.begin(GPSBaud);
@@ -47,14 +43,32 @@ void GPS::update(uint32_t now) {
 
             if (tiny_gps.date.isValid()) {
                 _date = tiny_gps.date;
+                _last_date_update = now;
             }
 
             if (tiny_gps.time.isValid()) {
                 _time = tiny_gps.time;
+                _last_time_update = now;
+            }
+
+            if (tiny_gps.speed.isValid()) {
+                _speed = tiny_gps.speed;
+                _last_speed_update = now;
             }
         }
     }
 }
 
-void GPS::pingGPS() {
+bool GPS::status(uint32_t now) {
+    LastUpdateArray lastUpdates = getUpdateTimes(now);
+    uint32_t* ptr = &lastUpdates.time_since_last_loc_update;
+    int numMembers = sizeof(LastUpdateArray) / sizeof(uint32_t);
+
+    for (int i = 0; i < numMembers; i++) {
+        if (*ptr > MAX_GPS_TIMEOUT) {
+            return false;
+        }
+        ptr++;
+    }
+    return true;
 }
