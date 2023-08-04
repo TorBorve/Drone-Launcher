7c7bfb7c8b4acd17c57c45b523b0bf76f7b036b1
Tor Børve Rasmussen <rasmustor18@gmail.com>
2023-08-04 15:43:14 +0200

    cleaning up and simplifying

--- embedded_sw/include/Navigator.h
+++ embedded_sw/include/Navigator.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <Adafruit_BNO08x.h>
+#include <TinyGPSPlus.h>
 #include <stdint.h>
 
-#include "GPS.h"
-#include "IMU.h"
 // Define data structures for each sensor type
 typedef struct euler_t {
     float yaw;
@@ -20,19 +19,19 @@ typedef struct quaternion_t {
 
 class IMU {
    public:
+    IMU();
     void update(uint32_t now);
     void init();
     euler_t getEuler(bool degrees);
     quaternion_t getQuat();
-    bool status(uint32_t now);
-    IMU();
 
    private:
-    uint32_t _prevUpdate;
-    sh2_RotationVectorWAcc_t* _quaternion;
-    void enableAll();
+    void enableReports();
     void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false);
     void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr, bool degrees = false);
+    Adafruit_BNO08x _imuSensor;
+    sh2_SensorValue_t _sensorValue;
+    uint32_t _prevUpdate;
 };
 
 typedef struct PositionArray {
@@ -41,61 +40,33 @@ typedef struct PositionArray {
     double alt;  // meters
 } PositionArray;
 
-typedef struct LastUpdateArray {
-    uint32_t time_since_last_loc_update;
-    uint32_t time_since_last_alt_update;
-    uint32_t time_since_last_date_update;
-    uint32_t time_since_last_time_update;
-    uint32_t time_since_last_speed_update;
-} LastUpdateArray;
-
 class GPS {
-   private:
-    uint32_t _prevUpdate;
-    double _lat;
-    double _long;
-    uint32_t _last_loc_update;
-    double _alt;  // meters
-    uint32_t _last_alt_update;
-    TinyGPSPlus tiny_gps;
-    TinyGPSDate _date;
-    uint32_t _last_date_update;
-    TinyGPSTime _time;
-    uint32_t _last_time_update;
-    TinyGPSSpeed _speed;
-    uint32_t _last_speed_update;
-
    public:
-    void update(uint32_t now);
-    void init();
-    bool status(uint32_t now);
-    PositionArray getPos() { return PositionArray{_lat,
-                                                  _long,
-                                                  _alt}; }
-    LastUpdateArray getUpdateTimes(uint32_t now) { return LastUpdateArray{now - _last_loc_update,
-                                                                          now - _last_alt_update,
-                                                                          now - _last_date_update,
-                                                                          now - _last_time_update,
-                                                                          now - _last_speed_update}; }
-    TinyGPSTime getTime() { return _time; }
-    TinyGPSDate getDate() { return _date; }
     GPS();
+    void init();
+    void update(uint32_t now);
+    PositionArray getPos();
+    bool getGPSFix();
+   private:
+    TinyGPSPlus _tinyGPS;
+    bool _gpsFix;
+    uint32_t _prevUpdate;
+
 };
 
-class GuidanceSystem {
+class Navigator {
+   public:
+    Navigator();
+    void init();
+    void update(uint32_t now);
+    PositionArray getPos();
+    bool getGPSFix();
+    euler_t getOrientationEul(bool degrees);
+    quaternion_t getOrientationQuat();
    private:
     GPS _gps;
     IMU _imu;
 
-   public:
-    void update(uint32_t now);
-    void init();
-    bool status(uint32_t now);
-    PositionArray getPos() { return _gps.getPos(); }
-    euler_t getOrientation(bool degrees) { return _imu.getEuler(degrees); }
-    quaternion_t getOrientationQuat() { return _imu.getQuat(); }
-    TinyGPSTime getTime() { return _gps.getTime(); }
-    TinyGPSDate getDate() { return _gps.getDate(); }
 };
 
-extern GuidanceSystem guidanceSystem;
+extern Navigator navigator;
--- embedded_sw/src/Navigator.cpp
+++ embedded_sw/src/Navigator.cpp
@@ -1,82 +1,46 @@
 #include "Navigator.h"
 
-#define BNO08X_RESET -1
-#define UPDATE_INTERVAL 1000
+#include "Log.h"
 
-// IMU imu;
-Adafruit_BNO08x bno08x(BNO08X_RESET);
-sh2_SensorValue_t sensorValue;
-euler_t ypr;
+#define IMU_BNO08X_RESET -1
+#define IMU_UPDATE_INTERVAL 10
 
-IMU::IMU() : _prevUpdate(0) {
+#define GPS_UPDATE_INTERVAL 500
+#define GPS_BAUD 38400
+#define GPS_SERIAL Serial4
+#define GPS_MAX_TIMEOUT 10000
+
+Navigator navigator{};
+
+IMU::IMU() : _imuSensor{IMU_BNO08X_RESET}, _prevUpdate{0} {
 }
 
 void IMU::init() {
-    if (!bno08x.begin_I2C()) {
-        // if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte
-        // UART buffer! if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-        Serial.println("Failed to find BNO08x chip");
+    if (!_imuSensor.begin_I2C()) {
+        LOG_ERROR("Failed to find BNO08x chip");
+        while (1)
+            ;
     }
-    enableAll();
+    enableReports();
 }
 
-void IMU::enableAll() {
-    if (!bno08x.enableReport(SH2_ARVR_STABILIZED_RV, 5000)) {
+void IMU::enableReports() {
+    if (!_imuSensor.enableReport(SH2_ARVR_STABILIZED_RV, IMU_UPDATE_INTERVAL * 1000)) {
         Serial.println("Could not enable rotation vector");
     }
-
-    /*
-        if (!bno08x.enableReport(SH2_GEOMAGNETIC_ROTATION_VECTOR)) {
-        Serial.println("Could not enable geomagnetic rotation vector");
-    }
-    if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-        Serial.println("Could not enable game rotation vector");
-    }
-    if (!bno08x.enableReport(sh2_Quaternion)) {
-        Serial.println("Could not enable game rotation vector");
-    }
-    if (!bno08x.enableReport(SH2_ACCELEROMETER)) {
-    Serial.println("Could not enable accelerometer");
-  }
-  if (!bno08x.enableReport(SH2_GYROSCOPE_CALIBRATED)) {
-    Serial.println("Could not enable gyroscope");
-  }
-  if (!bno08x.enableReport(SH2_MAGNETIC_FIELD_CALIBRATED)) {
-    Serial.println("Could not enable magnetic field calibrated");
-  }
-  if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION)) {
-    Serial.println("Could not enable linear acceleration");
-  }
-  if (!bno08x.enableReport(SH2_GRAVITY)) {
-    Serial.println("Could not enable gravity vector");
-  }
-
-  if (!bno08x.enableReport(SH2_STABILITY_CLASSIFIER)) {
-    Serial.println("Could not enable stability classifier");
-  }
-  if (!bno08x.enableReport(SH2_RAW_ACCELEROMETER)) {
-    Serial.println("Could not enable raw accelerometer");
-  }
-  if (!bno08x.enableReport(SH2_RAW_GYROSCOPE)) {
-    Serial.println("Could not enable raw gyroscope");
-  }
-  if (!bno08x.enableReport(SH2_RAW_MAGNETOMETER)) {
-    Serial.println("Could not enable raw magnetometer");
-  }*/
 }
 
 void IMU::update(uint32_t now) {
-    if (now - _prevUpdate < UPDATE_INTERVAL) {
+    if (now - _prevUpdate < IMU_UPDATE_INTERVAL) {
         return;
     }
     _prevUpdate = now;
 
-    if (bno08x.wasReset()) {
-        Serial.print("IMU sensor was reset");
-        enableAll();
+    if (_imuSensor.wasReset()) {
+        enableReports();
     }
 
-    if (!bno08x.getSensorEvent(&sensorValue)) {
+    if (!_imuSensor.getSensorEvent(&_sensorValue)) {
         return;
     }
 }
@@ -103,98 +67,68 @@ void IMU::quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler
 }
 
 euler_t IMU::getEuler(bool degrees) {
-    quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, degrees);
+    euler_t ypr;
+    quaternionToEulerRV(&_sensorValue.un.arvrStabilizedRV, &ypr, degrees);
     return ypr;
 }
 
-quaternion_t IMU::getQuat() { return quaternion_t{sensorValue.un.arvrStabilizedRV.real,
-                                                  sensorValue.un.arvrStabilizedRV.i,
-                                                  sensorValue.un.arvrStabilizedRV.j,
-                                                  sensorValue.un.arvrStabilizedRV.k}; }
+quaternion_t IMU::getQuat() { return quaternion_t{_sensorValue.un.arvrStabilizedRV.real,
+                                                  _sensorValue.un.arvrStabilizedRV.i,
+                                                  _sensorValue.un.arvrStabilizedRV.j,
+                                                  _sensorValue.un.arvrStabilizedRV.k}; }
 
-bool IMU::status(uint32_t now) {
-    // todo fix this
-    return true;
-}
-
-#include <SoftwareSerial.h>
-#define UPDATE_INTERVAL 1000
-#define GPSBaud 38400
-#define GPS_SERIAL Serial4
-#define MAX_GPS_TIMEOUT 10000
-
-GPS gps;
-
-GPS::GPS() : _prevUpdate(0), _lat(0), _long(0), _last_loc_update(0), _alt(0), _last_alt_update(0), tiny_gps(), _date(), _last_date_update(0), _time(), _last_time_update(0), _speed(), _last_speed_update(0) {
-}
+GPS::GPS() : _gpsFix{false}, _prevUpdate{0} {}
 
 void GPS::init() {
-    GPS_SERIAL.begin(GPSBaud);
+    GPS_SERIAL.begin(GPS_BAUD);
 }
 
 void GPS::update(uint32_t now) {
-    if (now - _prevUpdate < UPDATE_INTERVAL) {
+    for (int i = 0; i < 64 && GPS_SERIAL.available(); i++) {
+        _gpsFix = _tinyGPS.encode(GPS_SERIAL.read());
+    }
+    if (now - _prevUpdate < GPS_UPDATE_INTERVAL) {
         return;
     }
     _prevUpdate = now;
-    if (GPS_SERIAL.available() > 0) {
-        if (tiny_gps.encode(GPS_SERIAL.read())) {
-            if (tiny_gps.location.isValid()) {
-                _lat = tiny_gps.location.lat();
-                _long = tiny_gps.location.lng();
-                _last_loc_update = now;
-            }
-
-            if (tiny_gps.altitude.isValid()) {
-                _alt = tiny_gps.altitude.meters();
-                _last_alt_update = now;
-            }
-
-            if (tiny_gps.date.isValid()) {
-                _date = tiny_gps.date;
-                _last_date_update = now;
-            }
-
-            if (tiny_gps.time.isValid()) {
-                _time = tiny_gps.time;
-                _last_time_update = now;
-            }
-
-            if (tiny_gps.speed.isValid()) {
-                _speed = tiny_gps.speed;
-                _last_speed_update = now;
-            }
-        }
-    }
 }
 
-bool GPS::status(uint32_t now) {
-    LastUpdateArray lastUpdates = getUpdateTimes(now);
-    uint32_t* ptr = &lastUpdates.time_since_last_loc_update;
-    int numMembers = sizeof(LastUpdateArray) / sizeof(uint32_t);
-
-    for (int i = 0; i < numMembers; i++) {
-        if (*ptr > MAX_GPS_TIMEOUT) {
-            return false;
-        }
-        ptr++;
+PositionArray GPS::getPos() {
+    PositionArray pos{0, 0, 0};
+    if (_tinyGPS.location.isValid()) {
+        pos.lat = _tinyGPS.location.lat();
+        pos.lng = _tinyGPS.location.lng();
+        pos.alt = _tinyGPS.altitude.meters();
     }
-    return true;
+    return pos;
 }
 
-// GuidanceSystem::GuidanceSystem() : _gps(), _imu() {
-//     }
+bool GPS::getGPSFix() { return _gpsFix; }
 
-void GuidanceSystem::init() {
+Navigator::Navigator() :  _gps{}, _imu{} {}
+
+void Navigator::init() {
     _imu.init();
     _gps.init();
 }
 
-void GuidanceSystem::update(uint32_t now) {
+void Navigator::update(uint32_t now) {
     _imu.update(now);
     _gps.update(now);
 }
 
-bool GuidanceSystem::status(uint32_t now) {
-    return _imu.status(now) && _gps.status(now);
+PositionArray Navigator::getPos() {
+    return _gps.getPos();
+}
+
+bool Navigator::getGPSFix() {
+    return _gps.getGPSFix();
+}
+
+euler_t Navigator::getOrientationEul(bool degrees) {
+    return _imu.getEuler(degrees);
+}
+
+quaternion_t Navigator::getOrientationQuat() {
+    return _imu.getQuat();
 }
